32a23a99ca3922b5131762accdab36e428b49cab
Audun Sektnan <sek@nr.no>
2024-09-25 09:23:33 +0200

    Release v0.7.0

--- src/ccs_scripts/co2_plume_tracking/co2_plume_tracking.py
+++ src/ccs_scripts/co2_plume_tracking/co2_plume_tracking.py
@@ -160,7 +160,7 @@ def _setup_log_configuration(arguments: argparse.Namespace) -> None:
 
 def _log_input_configuration(arguments: argparse.Namespace) -> None:
     version = "v0.7.0"
-    is_dev_version = True
+    is_dev_version = False
     if is_dev_version:
         version += "_dev"
         try:
