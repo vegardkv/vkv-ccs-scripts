8fb8ad87c3efbc1c19c29cc90d47c3107087dc11
Audun Sektnan <sek@nr.no>
2025-02-03 14:16:29 +0100

    Release v0.8.0

--- src/ccs_scripts/co2_plume_area/co2_plume_area.py
+++ src/ccs_scripts/co2_plume_area/co2_plume_area.py
@@ -198,7 +198,7 @@ def _read_args() -> Tuple[str, str]:
 
 def _log_input_configuration(input_path: str, output_path: str) -> None:
     version = "v0.8.0"
-    is_dev_version = True
+    is_dev_version = False
     if is_dev_version:
         version += "_dev"
         try:
