3c5dcc7e1387b048030c0cbe2dd0893569890911
Vegard Berg Kvernelv <vkvernelv@gmail.com>
2025-06-03 15:39:06 +0200

    CCS-Arrow converter (#309)
    * Add script for csv-arrow conversion
    
    Added a python module that can be used as a script as well.
    There are four methods for handling csv-arrow conversion. One which
    is aimed directly at FMU realizations, and three others for general
    purposes. The latter are more convenient for testing.
    
    #215
    
    * Rename functions
    
    * Adapt converter main to ERT
    
    * Fix issues with co2_csv_arrow_converter main
    
    * Fix formatting and test error
    
    * Add arguments to ERT config
    
    * Add docs for csv-arrow converter
    
    * Fix rst error
    
    * Add DESCRIPTION to converter
    
    * Standardize names
    
    * Fix wrong import statement
    
    * Replace StrEnum with Enum

---
+++ src/ccs_scripts/co2_csv_arrow_converter/co2_csv_arrow_converter.py
@@ -0,0 +1,244 @@
+# This scripts checks all FMU realizations for missing arrow or csv files
+# representing plume extent, area or containment data. If one exists, but
+# not the other, it will create the missing file.
+
+import datetime
+from enum import Enum
+from pathlib import Path
+from typing import List
+
+import pandas as pd
+import pyarrow as pa
+
+DESCRIPTION = """This scripts checks all FMU realizations for missing arrow or
+csv files representing plume extent, area or containment data. If one exists,
+but not the other, it will create the missing file."""
+
+CATEGORY = "modelling.reservoir"
+
+
+def try_convert_csv_to_arrow(
+    csv_path: Path,
+    arrow_path: Path,
+    force_overwrite: bool = False,
+) -> bool:
+    """
+    Convert a CSV file to an Arrow file if the Arrow file does not exist or
+    if the force_overwrite flag is set to True.
+    """
+    if not csv_path.exists():
+        return False
+    if arrow_path.exists() and not force_overwrite:
+        return False
+
+    df = pd.read_csv(csv_path).rename(columns={"date": "DATE"})
+    _write_data_frame_to_arrow(df, arrow_path)
+    return True
+
+
+def try_convert_arrow_to_csv(
+    csv_path: Path,
+    arrow_path: Path,
+    force_overwrite: bool = False,
+) -> bool:
+    """
+    Convert an Arrow file to a CSV file if the CSV file does not exist or
+    if the force_overwrite flag is set to True.
+    """
+    if not arrow_path.exists():
+        return False
+    if csv_path.exists() and not force_overwrite:
+        return False
+
+    df = _read_data_frame_from_arrow(arrow_path)
+    df.to_csv(csv_path, index=False)
+    return True
+
+
+def try_convert_containment_csv_to_arrow(
+    csv_path: Path,
+    arrow_path: Path,
+    kept_columns: List[str],
+    force_overwrite: bool = False,
+) -> bool:
+    """
+    Convert a CSV file to an Arrow file for containment data if the Arrow
+    file does not exist or if the force_overwrite flag is set to True.
+    The CSV file is expected to have a "date" column and an "amount" column.
+    The Arrow file will have a "DATE" column and one column for each unique
+    combination of the kept columns, with the sum of the "amount" column
+    for each combination.
+    """
+    if not csv_path.exists():
+        return False
+    if arrow_path.exists() and not force_overwrite:
+        return False
+
+    raw_df = pd.read_csv(csv_path)
+    entries = []
+    for date, gf0 in raw_df.groupby("date"):
+        data = {
+            "DATE": date,
+        }
+        for keys, gf1 in gf0.groupby(kept_columns):
+            data["amount--" + "--".join(keys)] = gf1["amount"].sum()
+        entries.append(data)
+
+    df = pd.DataFrame(entries)
+    _write_data_frame_to_arrow(df, arrow_path)
+    return True
+
+
+def apply_to_realizations(
+    root_dir: Path,
+    realization_pattern: str,
+    kept_columns: List[str],
+    overwrite_arrow: bool = False,
+    overwrite_csv: bool = False,
+) -> None:
+    """
+    Apply the conversion functions to all realizations matching the
+    realization_pattern. The pattern should be a glob pattern relative to
+    the current directory. The function will create missing Arrow files
+    for CSV files and vice versa. The overwrite_arrow and overwrite_csv
+    flags control whether existing files should be overwritten.
+    """
+    assert not overwrite_csv or not overwrite_arrow
+    for realization_dir in root_dir.glob(realization_pattern):
+        # Extract paths for CSV and Arrow files
+        csv_path_1 = _get_csv_path(realization_dir, _FileType.PLUME_EXTENT)
+        csv_path_2 = _get_csv_path(realization_dir, _FileType.PLUME_AREA)
+        csv_path_3 = _get_csv_path(realization_dir, _FileType.CONTAINMENT)
+        arrow_path_1 = _get_arrow_path(realization_dir, _FileType.PLUME_EXTENT)
+        arrow_path_2 = _get_arrow_path(realization_dir, _FileType.PLUME_AREA)
+        arrow_path_3 = _get_arrow_path(realization_dir, _FileType.CONTAINMENT)
+
+        conversions = 0
+        # Try creating missing Arrow files for all realizations
+        conversions += try_convert_csv_to_arrow(
+            csv_path_1, arrow_path_1, overwrite_arrow
+        )
+        conversions += try_convert_csv_to_arrow(
+            csv_path_2, arrow_path_2, overwrite_arrow
+        )
+        conversions += try_convert_containment_csv_to_arrow(
+            csv_path_3, arrow_path_3, kept_columns, overwrite_arrow
+        )
+
+        # Try creating missing CSV files for all realizations
+        conversions += try_convert_arrow_to_csv(csv_path_1, arrow_path_1, overwrite_csv)
+        conversions += try_convert_arrow_to_csv(csv_path_2, arrow_path_2, overwrite_csv)
+        # No conversion for containment data from Arrow to CSV yet
+
+        print(f"Processed {realization_dir}: {conversions} conversions made.")
+
+
+class _FileType(Enum):
+    PLUME_EXTENT = "plume_extent"
+    PLUME_AREA = "plume_area"
+    CONTAINMENT = "containment"
+
+
+def _write_data_frame_to_arrow(df: pd.DataFrame, arrow_path: Path) -> None:
+    # Extract the timestamp in milliseconds from df. We must use the
+    # datetime module, since pandas cannot handle dates outside the range of
+    # 1677-09-21 to 2262-04-11. Afterwards we need to explicitly define the
+    # schema, since we cannot create a data frame that is directly convertible to
+    # Arrow.
+    dates = [
+        datetime.datetime(*[int(t) for t in d.split("-")])  # type: ignore[arg-type]
+        for d in df["DATE"]
+    ]
+    dates_ms = [
+        (d - datetime.datetime(1970, 1, 1)).total_seconds() * 1000 for d in dates
+    ]
+    non_date_df = df.drop(columns=["DATE"])
+
+    fields = [pa.field("DATE", pa.timestamp("ms"))]
+    for col in non_date_df.columns:
+        # Use default type inference for other columns
+        fields.append(pa.field(col, pa.infer_type(non_date_df[col])))
+
+    arrays = [dates_ms]
+    for col in non_date_df.columns:
+        arrays.append(pa.array(non_date_df[col]))
+
+    schema = pa.schema(fields)
+
+    # Create the table with the specified schema
+    table = pa.Table.from_arrays(arrays, schema=schema)
+    with pa.ipc.new_file(arrow_path, schema) as writer:
+        writer.write_table(table)
+
+
+def _get_csv_path(realization_dir: Path, file_type: _FileType) -> Path:
+    full_path = realization_dir / "share" / "results" / "tables"
+    if file_type == _FileType.PLUME_EXTENT:
+        full_path = full_path / "plume_extent.csv"
+    elif file_type == _FileType.PLUME_AREA:
+        full_path = full_path / "plume_area.csv"
+    elif file_type == _FileType.CONTAINMENT:
+        full_path = full_path / "plume_mass.csv"
+    else:
+        raise ValueError(f"Unknown file type: {file_type}")
+    return full_path
+
+
+def _get_arrow_path(realization_dir: Path, file_type: _FileType) -> Path:
+    return _get_csv_path(realization_dir, file_type).with_suffix(".arrow")
+
+
+def _read_data_frame_from_arrow(arrow_path: Path) -> pd.DataFrame:
+    with pa.ipc.open_file(arrow_path) as f:
+        table = f.read_all()
+    return table.to_pandas()
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        description="Create missing Arrow/CSV files for FMU realizations"
+    )
+    parser.add_argument(
+        "--root-dir",
+        type=Path,
+        help="Root directory for the glob pattern",
+        default=Path(".").resolve(),
+    )
+    parser.add_argument(
+        "--realization-pattern",
+        type=str,
+        help="Glob pattern (relative to root_dir) for an FMU realization directory",
+        default="realization-*/iter-*",
+    )
+    parser.add_argument(
+        "--kept-columns",
+        type=str,
+        help="Comma-separated list of columns to keep when converting containment data",
+        default="phase,containment",
+    )
+    parser.add_argument(
+        "--force-arrow-overwrite",
+        type=bool,
+        default=False,
+        help="Overwrite existing Arrow files even if they already exist",
+    )
+    parser.add_argument(
+        "--force-csv-overwrite",
+        type=bool,
+        default=False,
+        help="Overwrite existing CSV files even if they already exist",
+    )
+    args = parser.parse_args()
+    apply_to_realizations(
+        args.root_dir,
+        args.realization_pattern,
+        args.kept_columns.split(","),
+        args.force_arrow_overwrite,
+        args.force_csv_overwrite,
+    )
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_co2_csv_arrow_converter.py
@@ -0,0 +1,117 @@
+import io
+import tempfile
+from pathlib import Path
+
+import pandas as pd
+import pyarrow as pa
+from pytest import fixture
+
+from ccs_scripts.co2_csv_arrow_converter.co2_csv_arrow_converter import (
+    try_convert_arrow_to_csv,
+    try_convert_containment_csv_to_arrow,
+    try_convert_csv_to_arrow,
+)
+
+
+@fixture
+def mock_data_frame():
+    csv_text = """date,all_SGAS,amethyst_SGAS,ruby_SGAS,topaz_SGAS,all_AMFG,amethyst_AMFG,ruby_AMFG,topaz_AMFG,all_XMF2,amethyst_XMF2,ruby_XMF2,topaz_XMF2
+2025-01-01,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
+2027-01-01,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0
+2031-01-01,1960100.0,1960100.0,1140000.0,1159100.0,8758100.0,8635100.0,8409800.0,8658100.0,8605100.0,7995200.0,7787800.0,8562000.0
+2033-01-01,2538900.0,2538900.0,1214900.0,1190100.0,8788100.0,8676100.0,8487900.0,8678100.0,8728100.0,8535000.0,8223700.0,8665000.0
+2225-01-01,3708100.0,3588400.0,1207700.0,1231000.0,8809100.0,8747100.0,8547700.0,8760100.0,8620900.0,8346300.0,8208300.0,8528900.0
+2250-01-01,3716100.0,3596400.0,1208600.0,1212000.0,8809100.0,8747100.0,8547700.0,8760100.0,8685100.0,8513400.0,8311100.0,8604000.0
+2300-01-01,3771100.0,3633400.0,1246700.0,1211000.0,8809100.0,8757100.0,8557700.0,8760100.0,8607800.0,8363400.0,8208400.0,8463900.0
+2400-01-01,3864100.0,3773300.0,1244800.0,1201000.0,8809100.0,8757100.0,8557700.0,8770100.0,8627900.0,8423400.0,8190300.0,8501100.0
+2450-01-01,3913900.0,3813200.0,1253800.0,1207100.0,8809100.0,8757100.0,8557700.0,8770100.0,8607900.0,8363300.0,8150100.0,8480800.0
+2500-01-01,3943800.0,3832200.0,1234800.0,1217000.0,8809100.0,8757100.0,8557700.0,8770100.0,8675900.0,8476400.0,8223100.0,8588800.0
+"""  # noqa: E501
+    return pd.read_csv(io.StringIO(csv_text))
+
+
+@fixture
+def mock_containment_data_frame():
+    return pd.read_csv(
+        Path(__file__).parent / "testdata_co2_tables" / "plume_mass.csv",
+        parse_dates=["date"],
+    )
+
+
+def test_convert_csv_to_arrow(mock_data_frame):
+    # Dump mock data frame to CSV in a temporary file in a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        csv_path = Path(temp_dir) / "test.csv"
+        arrow_path = Path(temp_dir) / "test.arrow"
+        mock_data_frame.to_csv(csv_path, index=False)
+
+        # Test conversion
+        assert try_convert_csv_to_arrow(csv_path, arrow_path)
+        assert arrow_path.exists()
+
+        # Test that the Arrow file is not empty
+        table = pa.ipc.open_file(arrow_path).read_all()
+        assert table.num_rows == len(mock_data_frame)
+        assert table.num_columns == len(mock_data_frame.columns)
+
+        # Test that conversion is not done again...
+        assert not try_convert_csv_to_arrow(csv_path, arrow_path)
+
+        # ... unless forced
+        assert try_convert_csv_to_arrow(csv_path, arrow_path, force_overwrite=True)
+        # Test that the Arrow file is not empty after forced conversion
+        table = pa.ipc.open_file(arrow_path).read_all()
+        assert table.num_rows == len(mock_data_frame)
+        assert table.num_columns == len(mock_data_frame.columns)
+
+
+def test_convert_arrow_to_csv(mock_data_frame):
+    # Dump mock data frame to Arrow in a temporary file in a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        csv_path = Path(temp_dir) / "test.csv"
+        arrow_path = Path(temp_dir) / "test.arrow"
+
+        # Dump data frame to arrow format
+        table = pa.Table.from_pandas(mock_data_frame)
+        with pa.ipc.new_file(arrow_path, table.schema) as writer:
+            writer.write_table(table)
+        assert arrow_path.exists()
+        assert try_convert_arrow_to_csv(csv_path, arrow_path)
+        assert csv_path.exists()
+
+        # Test that the CSV file is not empty
+        df = pd.read_csv(csv_path)
+        assert not df.empty
+        assert df.shape == mock_data_frame.shape
+
+        # Test that conversion is not done again...
+        assert not try_convert_arrow_to_csv(csv_path, arrow_path)
+
+        # ... unless forced
+        assert try_convert_arrow_to_csv(csv_path, arrow_path, force_overwrite=True)
+        # Test that the CSV file is not empty after forced conversion
+        df = pd.read_csv(csv_path)
+        assert not df.empty
+        assert df.shape == mock_data_frame.shape
+
+
+def test_convert_containment_csv_to_arrow(mock_containment_data_frame):
+    # Dump mock data frame to CSV in a temporary file in a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        csv_path = Path(temp_dir) / "test.csv"
+        arrow_path = Path(temp_dir) / "test.arrow"
+        mock_containment_data_frame.to_csv(csv_path, index=False)
+
+        # Test conversion
+        assert try_convert_containment_csv_to_arrow(
+            csv_path,
+            arrow_path,
+            kept_columns=["phase", "containment"],
+        )
+        assert arrow_path.exists()
+
+        # Test that the Arrow file is not empty
+        # The data will have transformed and the shape will be different
+        table = pa.ipc.open_file(arrow_path).read_all()
+        assert table.num_rows > 0
+        assert table.num_columns > mock_containment_data_frame.shape[1]
