55379130be028749cd7eae128c67f1382d82702a
Audun Sektnan <99661468+AudunSektnanNR@users.noreply.github.com>
2025-08-22 11:10:17 +0200

    Temp remove new code for depleted fields, to be consistent with current version in webviz-subsurface (#352)
    * Fix merge
    
    * Fix lint

--- src/ccs_scripts/aggregate/_config.py
+++ src/ccs_scripts/aggregate/_config.py
@@ -93,11 +93,9 @@ class CO2MassSettings:
     init_source: str
     maps: Optional[List[str]] = None
     residual_trapping: bool = False
-    gas_molar_mass: Optional[float] = None
 
     def __post_init__(self):
-        if self.gas_molar_mass is not None:
-            self.gas_molar_mass = float(self.gas_molar_mass)
+        pass
 
 
 @dataclass
