8c428a88494ea6c5f5ffc407c8cd2436bc633f71
Jorge Sicacha <sicacha@nr.no>
2026-04-10 11:20:17 +0200

    Release version v0.15.0

--- src/ccs_scripts/co2_plume_extent/co2_plume_extent.py
+++ src/ccs_scripts/co2_plume_extent/co2_plume_extent.py
@@ -544,7 +544,7 @@ def _setup_log_configuration(arguments: argparse.Namespace) -> None:
 
 def _log_input_configuration(args: argparse.Namespace) -> None:
     version = "v0.15.0"
-    is_dev_version = True
+    is_dev_version = False
     if is_dev_version:
         version += "_dev"
         try:
