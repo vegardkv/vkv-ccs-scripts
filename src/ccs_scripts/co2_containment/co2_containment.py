a90af7f31ac5ecea1ec26197f0031c61cb20b303
Jorge Sicacha <sicacha@nr.no>
2026-04-10 09:49:47 +0200

    Release version v0.15.0

--- src/ccs_scripts/co2_containment/co2_containment.py
+++ src/ccs_scripts/co2_containment/co2_containment.py
@@ -687,7 +687,7 @@ def log_input_configuration(args: argparse.Namespace) -> None:
     Log the provided input
     """
     version = "v0.15.0"
-    is_dev_version = True
+    is_dev_version = False
     if is_dev_version:
         version += "_dev"
         try:
